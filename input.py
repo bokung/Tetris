7e4aaaadbc14777b54f7597a16ffb4bf10b0359f
bokung <e0774428@u.nus.edu>
2022-12-29 18:25:17 +0800

    Approaches

--- input.py
+++ input.py
@@ -1,3 +1,8 @@
 '''
 First Problem: How to read input from Tetr.io and convert it to a usable tetris board in our local machine?
+Possible Approaches:
+> Somehow read web elements directly?
+> Keep capturing the screen at every frame and use an algorithm to convert it to a usable representation?
+> Use Tetrio api (Is there even one?)
+> Turn on audio cues in settings at use sounds to determine next piece?
 '''
