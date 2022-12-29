4de3cb328d0f0f0526226f6848505b8d1b2ffd22
bokung <e0774428@u.nus.edu>
2022-12-29 19:46:50 +0800

    Update input.py

--- input.py
+++ input.py
@@ -6,6 +6,7 @@ Possible Approaches:
 > Use Tetrio api (Is there even one?)
 > Turn on audio cues in settings at use sounds to determine next piece?
 > Read web socket packages and somehow extract data about board state (how to do?)
+> Download desktop version and read memory locations that give current board state
 
 Interface input(int row, int col) {
     board => np.array((row, col)) # board[i][j] = 0 if its an empty space, 1 if it has a block. We don't need information about colour of every square.
