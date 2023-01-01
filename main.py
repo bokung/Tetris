07ace6765a77efab612fe72e94020182f3c8e5ac
bokung <e0774428@u.nus.edu>
2023-01-01 18:33:09 +0800

    Migrate to python again

---
+++ main.py
@@ -0,0 +1,29 @@
+'''
+First Problem: How to read input from Tetr.io and convert it to a usable tetris board in our local machine?
+Possible Approaches:
+> Somehow read web elements directly?
+> Keep capturing the screen at every frame and use an algorithm to convert it to a usable representation?
+> Use Tetrio api (Is there even one?)
+> Turn on audio cues in settings at use sounds to determine next piece?
+> Read web socket packages and somehow extract data about board state (how to do?)
+> Download desktop version and read memory locations that give current board state
+
+Interface input(int row, int col) {
+    board => np.array((row, col)) # board[i][j] = 0 if its an empty space, 1 if it has a block. We don't need information about colour of every square.
+    hold = some tetris piece that we are holding, initialise to null
+    next = LinkedList, everytime we put down a piece we append next piece to end of list, and set new head to be the next element.
+}
+
+Working Methods:
+misterhat: Set fixed sized window and read colours of pixels at predefined locations (hard coded, BAD!)
+           > Minor improvements: take screenshots but be able to determine where main board is without hard coding, find some relation between play area position and window size.
+             > Robotjs is too limited in functionality, hard to actually develop anything remotely general and robust.
+
+Confirmed direction:
+> Use OpenCV as input manager to create a resilient tetrio bot that doesn't break whenever the tetrio devs change board size, we change the window size or some other small thing.
+
+NO HARDCODING!
+'''
+
+import cv2 as cv
+import numpy as np
