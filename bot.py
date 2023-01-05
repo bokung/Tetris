6a2bff727bb8034971d861757c679a35c8a6d2e3
bokung <e0774428@u.nus.edu>
2023-01-05 15:59:19 +0800

    Specifications for input

---
+++ bot.py
@@ -0,0 +1,7 @@
+'''
+ASSUME INPUT WORKS CORRECTLY :)
+We are given the board as a (ROW, COLUMN) np.ndarray of True and False.
+True indicates that a block is there, False indicates an empty square.
+Also given is a list of the next 5 pieces, e.g. ['s','z','j','l','o']
+We also have a variable that keeps track of what the held piece is, e.g. HELD = 's'
+'''
